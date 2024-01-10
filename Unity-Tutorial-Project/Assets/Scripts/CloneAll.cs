364d13a5e75cc3c3634bbba8a57d6909ef65e5a2
2144619 <144230645+2144619@users.noreply.github.com>
2024-01-10 12:57:21 +0000

    RandomCloning, Arrays & More Cloning

---
+++ Unity-Tutorial-Project/Assets/Scripts/CloneAll.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneAll : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject toClone = null;
+
+    [SerializeField]
+    float spacing = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int cloneIndex = 0;
+
+        GameObject chosenToClone = null;
+
+        GameObject clonedObject = null;
+
+        Vector3 clonePos = Vector3.zero;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Unity-Tutorial-Project/Assets/Scripts/RandomClone.cs
+++ Unity-Tutorial-Project/Assets/Scripts/RandomClone.cs
@@ -17,10 +17,36 @@ public class RandomClone : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // INITIALIZE chosenIndex variable
         int chosenIndex = 0;
+
+        // SET chosenIndex to a randomly generated value between zero and the length of the toClone array
         chosenIndex = Random.Range(0, toClone.Length);
 
+        // INITIALIZE chosenToClone variable
         GameObject chosenToClone = null;
+
+        // SET chosenToClone to the chosenIndex GameObject in the toClone array
+        chosenToClone = toClone[chosenIndex];
+
+        // INITIALIZE clonedObject variable
+        GameObject clonedObject = null;
+
+        // CALL the method Instantiate(), using chosenToClone, and store the value in the clonedObject variable
+        clonedObject = Instantiate(chosenToClone);
+
+        // INITIALIZE randomPos variable
+        Vector3 randomPos = Vector3.zero;
+
+        // SET randomPos’ x value to a randomly generated value between minPos and maxPos
+        randomPos.x = Random.Range(minPos, maxPos);
+
+        // SET randomPos’ y value to a randomly generated value between minPos and maxPos
+        randomPos.y = Random.Range(minPos, maxPos);
+
+        // SET clonedObject’s transform’s position to randomPos
+        clonedObject.transform.position = randomPos;
+
 
 
 
